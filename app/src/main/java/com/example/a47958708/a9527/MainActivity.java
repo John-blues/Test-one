a32f46e9cf20b1d6a793d6d47944cc5b7c0ab957
lishuai <lishuai>
2016-09-11 17:55:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a47958708/a9527/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a47958708.a9527;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a847958708.new_first.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.a847958708.new_first.R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/a847958708/new_first/Newf_first.java
@@ -0,0 +1,13 @@
+package com.example.a847958708.new_first;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Newf_first extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newf_first);
+
+    }
+}
